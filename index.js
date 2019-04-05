0f1521af5f1b3bd1fcad35c0d1d37d7d2b592ab3
Krish <tuffleo@gmail.com>
2019-04-05 11:11:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const ua = require('universal-analytics');
 const {URL} = require('url');
 const gsearch = require('./helpers/gsearch.js');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 const GA_ACCOUNT = 'UA-114816386-1';
 const app = express();
 
